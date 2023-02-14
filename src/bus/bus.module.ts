d68b4416b2f162196338703062be2fcd1212b4d1
ghost-hikaru <melvin.mathurin35@gmail.com>
2023-02-14 11:14:20 +0100

    Mise en place de redis, ajout et getbyId fonctionnel

--- src/bus/bus.module.ts
+++ src/bus/bus.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-//import { redisModule } from 'src/modules.config'; RedisModule
+import { redisModule } from 'src/modules.config'; 
 import { BusController } from './bus.controller';
 import { BusService } from './bus.service';
 
 @Module({
-  //imports: [redisModule],
+  imports: [redisModule],
   controllers: [BusController],
   providers: [BusService]
 })
